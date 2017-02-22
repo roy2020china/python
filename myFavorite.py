50990aaa4f0c3296df7da646bd25a9cb8a19d138
zhang bing <40224272@qq.com>
2017-02-22 20:36:45 +0800

    Delete myFavorite.py

--- myFavorite.py
+++
@@ -1,4 +0,0 @@
-import movie
-
-myFavorite = movie.Movie('huangrong', 'a kongfu movie released in 1983', 'http://v.youku.com/v_show/id_XMjI3MDQ1MTU2.html', 'http://v.youku.com/v_show/id_XMjI3MDQ1MTU2.html')
-myFavorite.openmovietrailer()
