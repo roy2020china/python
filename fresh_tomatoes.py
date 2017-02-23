16d381b53d60d79a65b59227b80784dbd33099f8
roy2020china <40224272@qq.com>
2017-02-23 17:20:31 +0800

    add Procfile

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -16,6 +16,7 @@ main_page_head = '''
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
+            background-color: black;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
@@ -107,7 +108,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Python, jQuery, Bootstrap, HTML5, CSS3, Javascript</a>
           </div>
         </div>
       </div>
